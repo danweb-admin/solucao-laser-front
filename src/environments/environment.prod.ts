454eeae91ee7def2b4495a5ee110d03ded17dd2b
William Nascimento <william.nascimento2010@gmail.com>
2025-07-10 14:34:26 -0300

    k8s front

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  URL_API: 'http://solucao-back-svc.solucao.svc.cluster.local',
+  URL_API: 'http://95.111.226.231:32100',
   hmr: false,
   role: ''
 };
